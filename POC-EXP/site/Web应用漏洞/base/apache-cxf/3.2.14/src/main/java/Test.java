f7623335d40ceb5de208a313bf0cd4089bba1b2d
SecNN <34683107+SecNN@users.noreply.github.com>
2025-11-28 23:09:44 +0800

    Delete POC-EXP/site directory

--- POC-EXP/site/Web应用漏洞/base/apache-cxf/3.2.14/src/main/java/Test.java
+++
@@ -1,6 +0,0 @@
-import javax.jws.WebService;
-
-@WebService(name = "Test", targetNamespace = "http://service.namespace/")
-public interface Test {
-    String test(Model model);
-}
--- POC-EXP/site/Web应用漏洞/base/apache-cxf/3.2.14/src/main/java/TestImpl.java
+++
@@ -1,7 +0,0 @@
-public class TestImpl implements Test {
-
-    @Override
-    public String test(Model model) {
-        return model.toString();
-    }
-}
